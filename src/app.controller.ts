b01a28e84ed4332d9f225f94579b2d9ac56435b7
Bird <aflybird0@gmail.com>
2023-04-28 16:21:11 +0800

    feat: addexample with param and complex response
    Signed-off-by: Bird <aflybird0@gmail.com>

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 import { AppService } from './app.service';
 import { I18n, I18nContext, I18nLang, I18nService } from 'nestjs-i18n';
 import { time } from 'console';
@@ -18,5 +18,26 @@ export class AppController {
     return i18n.t('application.CREATE.TrailMaxLimit', {
       args: { limit: limit },
     });
+  }
+
+  /*
+  测试同时使用 param 路径参数，并且把国际化的文字放在复杂的返回体中
+  */
+  @Get('/i18n-complex/:param')
+  async i18nExampleComplex(
+    @Param('param') param: string,
+    @I18n() i18n: I18nContext,
+  ) {
+    const limit = 1;
+    console.log('param', param);
+    return {
+      code: 200,
+      msg:
+        param +
+        ' ' +
+        i18n.t('application.CREATE.TrailMaxLimit', {
+          args: { limit: limit },
+        }),
+    };
   }
 }
